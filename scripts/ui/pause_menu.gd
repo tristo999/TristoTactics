1c0e7fac2e521b40f0dc3917a1bc86dc9b85d69f
Tristan Fitzwater <tristanfitzwater@gmail.com>
2026-03-01 23:48:22 -0500

    Add Game Save and name Input Scene

--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -4,6 +4,8 @@ signal settings_requested
 
 func _setup_menu():
 	$Panel/VBox/ResumeButton.pressed.connect(_on_resume_pressed)
+	$Panel/VBox/RestartButton.pressed.connect(_on_restart_pressed)
+	$Panel/VBox/MainMenuButton.pressed.connect(_on_main_menu_pressed)
 	$Panel/VBox/SettingsButton.pressed.connect(_on_settings_pressed)
 	$Panel/VBox/QuitButton.pressed.connect(_on_quit_pressed)
 
@@ -16,3 +18,11 @@ func _on_resume_pressed():
 func _on_quit_pressed():
 	get_tree().paused = false
 	get_tree().quit()
+
+func _on_restart_pressed():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+func _on_main_menu_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/ui/MainMenu.tscn")
